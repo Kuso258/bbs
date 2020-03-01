742b042d283c5659d24ff5bc6701b164f75e63e7
parker <parker.fu@feisu.com>
2020-03-01 22:21:13 +0800

    分类列表

--- app/helpers.php
+++ app/helpers.php
@@ -2,3 +2,19 @@
 function route_class(){
     return str_replace('.',',',Route::currentRouteName());
 }
+
+function is_categories_active($paths,$number){
+    $paths = trim($paths,'/');
+    //对应topics路由
+    if($paths == "topics" && $number == 0){
+        return 'active';
+    }
+    //对应categories路由
+    $path = explode('/',$paths);
+    if(is_array($path) && count($path) == 2){
+        if($path[0] == "categories" && $path[1] == $number){
+            return "active";
+        }
+    }
+    return false;
+}
