c1eb79a9c07a541e1e7f776c3a0bc6e82da02759
parker <parker.fu@feisu.com>
2020-03-04 00:20:42 +0800

    话题修改

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -42,11 +42,16 @@ class TopicsController extends Controller
     }
 
     public function update(TopicRequest $request, Topic $topic){
-        return view();
+        $this->authorize('update', $topic);
+        $data = $request->all();
+        $topic->update($data);
+        return redirect()->route('topics.show',$topic->id)->with('success',"更新成功");
     }
 
-    public function edit(){
-        echo 1111;
+    public function edit(Topic $topic){
+        $this->authorize('update', $topic);
+        $categories = Category::all();
+        return view('topics.create_and_edit',compact('topic','categories'));
     }
 
     public function destory(){
---
+++ app/Policies/TopicPolicy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Topic;
+
+class TopicPolicy extends Policy
+{
+    public function update(User $user, Topic $topic)
+    {
+        return $topic->user_id == $user->id;
+    }
+}
