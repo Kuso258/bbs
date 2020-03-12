440130839d776726f465d336c438106836e6a47a
parker <parker.fu@feisu.com>
2020-03-12 23:39:58 +0800

    api测试

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'bindings',
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->name('api.v1.')->group(function() {
     Route::get('version', function() {
+        abort(403, 'test');
         return 'this is version v1';
     })->name('version');
 });
